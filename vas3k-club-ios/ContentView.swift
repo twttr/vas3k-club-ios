9c7642059738157a2aaf8c242faa8cade6cceba8
Pavel Romanishkin <pavel.romanishkin@payworks.com>
2020-10-13 20:55:12 +0200

    Initial Commit

---
+++ vas3k-club-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  vas3k-club-ios
+//
+//  Created by Romanishkin, Pavel on 13.10.20.
+//  Copyright © 2020 Romanishkin, Pavel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
