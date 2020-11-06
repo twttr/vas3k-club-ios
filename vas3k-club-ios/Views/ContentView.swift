e4cead4fec5d21486a3b1de9e7e4157ffda0eeb0
Pavel Romanishkin <pavel.romanishkin@payworks.com>
2020-11-06 20:26:38 +0100

    add pull to refresh to posts view

--- vas3k-club-ios/Views/ContentView.swift
+++ vas3k-club-ios/Views/ContentView.swift
@@ -8,16 +8,25 @@
 
 import SwiftUI
 import CoreData
+import SwiftUIRefresh
 
 struct ContentView: View {
     @ObservedObject var postsViewModel = PostsViewModel()
     @EnvironmentObject var viewRouter: ViewRouter
+    @State private var isShowing = false
     var body: some View {
         NavigationView {
             List(postsViewModel.postsList) { post in
                 NavigationLink(destination: PostView(postViewModel: PostViewModel(postId: post.id))) {
                     VStack(alignment: .leading) {
                         Text(post.title!)
+                    }
+                }
+            }.pullToRefresh(isShowing: $isShowing) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    self.isShowing = true
+                    postsViewModel.updateList { (_) in
+                        self.isShowing = false
                     }
                 }
             }
@@ -37,10 +46,15 @@ class PostsViewModel: ObservableObject {
     @Published var postsList: [Post] = [Post]()
     
     init() {
+        updateList {_ in }
+    }
+    
+    func updateList(completion: @escaping (Bool) -> ()) ->() {
         self.data.fetchPosts { (_) in
             DispatchQueue.main.async {
                 let fetchRequest = NSFetchRequest<Post>.init(entityName: "Post")
                 self.postsList = try! self.context.fetch(fetchRequest)
+                completion(true)
             }
         }
     }
