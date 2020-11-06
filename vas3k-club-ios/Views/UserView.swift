83d560b2de3718f0c20a8da6d5c7e1c3df9d5656
Pavel Romanishkin <pavel.romanishkin@payworks.com>
2020-11-06 19:53:07 +0100

    add logout

--- vas3k-club-ios/Views/UserView.swift
+++ vas3k-club-ios/Views/UserView.swift
@@ -7,10 +7,44 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct UserView: View {
+    @EnvironmentObject var viewRouter: ViewRouter
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button(action: { self.logout() })
+                { Text("Log Out") }
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+                .frame(width: 220, height: 60)
+                .background(Color(UIColor.blue))
+                .cornerRadius(15.0)
+        }.padding()
+    }
+    
+    func logout() -> (){
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Post")
+        let defaults = UserDefaults.standard
+        
+        if let _ = defaults.object(forKey: "token") as? String {
+            defaults.removeObject(forKey: "token")
+        }
+        do
+            {
+                let results = try context.fetch(request)
+                for managedObject in results
+                {
+                    let managedObjectData:NSManagedObject = managedObject as! NSManagedObject
+                    context.delete(managedObjectData)
+                }
+            } catch let error as NSError {
+                print(error)
+            }
+        self.viewRouter.currentPage = "LoginView"
     }
 }
 
